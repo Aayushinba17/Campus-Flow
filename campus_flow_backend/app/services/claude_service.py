7dedcf0cbb04fc4894a6c82005979f35af02a5a6
Aayushinba17 <aayushinba07@gmail.com>
2026-06-15 18:10:45 +0530

    Comment out api_version from Gemini Client

--- campus_flow_backend/app/services/claude_service.py
+++ campus_flow_backend/app/services/claude_service.py
@@ -26,7 +26,7 @@ class GeminiMessages:
 
         client = genai.Client(
             api_key=settings.GEMINI_API_KEY,
-            http_options=types.HttpOptions(api_version="v1"),
+            # http_options=types.HttpOptions(api_version="v1"),
         )
 
         contents = []
